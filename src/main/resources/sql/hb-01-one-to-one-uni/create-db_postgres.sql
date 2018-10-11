0d0f4a231021efee254ccefd6f43fbf6212fa527
Martin Keßel <m.kessel@skytron-energy.com>
2018-10-11 16:39:19 +0200

    First Commit

---
+++ src/main/resources/sql/hb-01-one-to-one-uni/create-db_postgres.sql
@@ -0,0 +1,24 @@
+DROP SCHEMA IF EXISTS hb_01_one_to_one_uni;
+
+CREATE SCHEMA hb_01_one_to_one_uni;
+
+SET SCHEMA 'hb_01_one_to_one_uni';
+
+DROP TABLE IF EXISTS instructor_detail;
+
+CREATE TABLE instructor_detail (
+  id serial primary key,
+  youtube_channel varchar(128) DEFAULT NULL,
+  hobby varchar(45) DEFAULT NULL
+);
+
+
+DROP TABLE IF EXISTS instructor;
+
+CREATE TABLE instructor (
+  id serial primary key,
+  first_name varchar(45) DEFAULT NULL,
+  last_name varchar(45) DEFAULT NULL,
+  email varchar(45) DEFAULT NULL,
+  instructor_detail_id int DEFAULT NULL
+);
